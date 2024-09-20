13685c839788371154dd759d59fb98d9a7ad7abf
Gowtham <GowthamPrakash.G@datayaan.com>
2024-09-20 13:29:46 +0530

    Added Provider in GetTableByHN,Prov,Db

--- SchemaCraftHub/Service/EntitySchemaService.cs
+++ SchemaCraftHub/Service/EntitySchemaService.cs
@@ -103,7 +103,8 @@ namespace SchemaCraftHub.Service
                         EntityName = table.EntityName,
                         AccessKey = table.AccessKey,
                         SecretKey = table.SecretKey,
-                        Region = table.Region
+                        Region = table.Region,
+                        Provider = table.Provider
                         // Map other properties as needed
                     }).ToList();
 
@@ -127,7 +128,8 @@ namespace SchemaCraftHub.Service
                         EntityName = table.EntityName,
                         Ec2Instance = table.Ec2Instance,
                         IPAddress = table.IPAddress,
-                        Keyspace = table.Keyspace
+                        Keyspace = table.Keyspace,
+                        Provider = table.Provider
                         // Map other properties as needed
                     }).ToList();
 
